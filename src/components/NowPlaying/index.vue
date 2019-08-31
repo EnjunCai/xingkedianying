9dc928aa896313ec4433d183378afe1bc66cb144
caienjun <1208823155@qq.com>
2019-08-31 14:01:34 +0800

    add getCity

--- src/components/NowPlaying/index.vue
+++ src/components/NowPlaying/index.vue
@@ -1,54 +1,192 @@
 <template>
-
-        <div class="movie_body">
-				<ul>
-					<li v-for="item in movieList" :key="item.id">
-						<div class="pic_show"><img :src="item.img | setWH('128.180')"></div>
-						<div class="info_list">
-							<h2>{{item.nm}}  <img v-if="item.version" src="@/assets/maxs.png" /></h2>
-							<p>观众评 <span class="grade">{{item.sc}}</span></p>
-							<p>主演: {{item.star}}</p>
-							<p>{{item.showInfo}}</p>
-						</div>
-						<div class="btn_mall">
-							购票
-						</div>
-					</li>
-				</ul>
-			</div>
-
+  <div class="movie_body">
+    <Loading v-if="isLoading"/>
+    <Scroll v-else :handleToScroll="handleToScroll" :handleToTouchEnd="handleToTouchEnd">
+      <ul>
+        <li class="pullDown">
+          <img src="../../assets/刷新.png" v-if="alll" alt />
+        </li>
+        <li v-for="item in movieList" :key="item.id">
+          <div class="pic_show" @tap="handleToDetail">
+            <img :src="item.img | setWH('128.180')" />
+          </div>
+          <div class="info_list">
+            <h2>
+              {{item.nm}}
+              <img v-if="item.version" src="@/assets/maxs.png" />
+            </h2>
+            <p>
+              观众评
+              <span class="grade">{{item.sc}}</span>
+            </p>
+            <p>主演: {{item.star}}</p>
+            <p>{{item.showInfo}}</p>
+          </div>
+          <div class="btn_mall">购票</div>
+        </li>
+      </ul>
+    </Scroll>
+  </div>
 </template>
 
 <script>
 export default {
-	name:'NowPlaying',
-	data(){
-		return {
-			movieList:[]
-		}
-	},
-	mounted() {
-		this.axios.get('/api/movieOnInfoList?cityId=10').then((res)=>{
-			var msg = res.data.msg;
-			if(msg=='ok'){
-				this.movieList = res.data.data.movieList
-			}
-		})
-	},
-}
+  name: "NowPlaying",
+  data() {
+    return {
+      movieList: [],
+      pullDownMsg: "",
+      alll: false,
+      isLoading:true,
+      prevCityId:-1
+    };
+  },
+  activated() {
+    var cityId = this.$store.state.city.id
+    if(this.prevCityId ===cityId){return;}
+    this.isLoading = true
+    this.axios.get("/api/movieOnInfoList?cityId="+cityId).then(res => {
+      var msg = res.data.msg;
+      if (msg == "ok") {
+        this.movieList = res.data.data.movieList;
+        this.isLoading=false
+        this.prevCityId = cityId
+        // this.$nextTick(()=>{
+        // 	var scroll = new BScroll(this.$refs.movie_body,{
+        // 		tap:true,
+        // 		probeType:1
+        // 	});
+        // 	scroll.on('scroll',(pos)=>{
+        // 		if(pos.y>30){
+        // 		this.alll=true
+        // 		}
+
+        // 	});
+        // 	scroll.on('touchEnd',(pos)=>{
+        // 		if(pos.y>30){
+        // 			this.axios.get('/api/movieOnInfoList?cityId=10').then((res)=>{
+        // 				var msg = res.data.msg;
+        // 				if(msg==='ok'){
+        // 				this.srcc='../../assets/成功-01.png'
+        // 				setTimeout(()=>{
+        // 					this.movieList = res.data.data.movieList
+        // 					this.alll=false
+        // 				},1000)
+
+        // 				}
+        // 			})
+        // 		}
+        // 	})
+        // })
+      }
+    });
+  },
+  methods: {
+    handleToDetail() {
+      console.log("ok");
+    },
+    handleToScroll(pos) {
+      if (pos.y > 30) {
+        this.alll = true;
+      }
+    },
+    handleToTouchEnd(pos) {
+      if (pos.y > 30) {
+        this.axios.get("/api/movieOnInfoList?cityId=10").then(res => {
+          var msg = res.data.msg;
+          if (msg === "ok") {
+            this.srcc = "../../assets/成功-01.png";
+            setTimeout(() => {
+              this.movieList = res.data.data.movieList;
+              this.alll = false;
+            }, 1000);
+          }
+        });
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
-    #content .movie_body{ flex:1; overflow:auto;}
-.movie_body ul{ margin:0 12px; overflow: hidden;}
-.movie_body ul li{ margin-top:12px; display: flex; align-items:center; border-bottom: 1px #e6e6e6 solid; padding-bottom: 10px;}
-.movie_body .pic_show{ width:64px; height: 90px;}
-.movie_body .pic_show img{ width:100%;}
-.movie_body .info_list { margin-left: 10px; flex:1; position: relative;}
-.movie_body .info_list h2{ font-size: 17px; line-height: 24px; width:150px; overflow: hidden; white-space: nowrap; text-overflow:ellipsis;}
-.movie_body .info_list p{ font-size: 13px; color:#666; line-height: 22px; width:200px; overflow: hidden; white-space: nowrap; text-overflow:ellipsis;}
-.movie_body .info_list .grade{ font-weight: 700; color: #faaf00; font-size: 15px;}
-.movie_body .info_list img{ width:50px; position: absolute; right:10px; top: 5px;}
-.movie_body .btn_mall , .movie_body .btn_pre{ width:47px; height:27px; line-height: 28px; text-align: center; background-color: #f03d37; color: #fff; border-radius: 4px; font-size: 12px; cursor: pointer;}
-.movie_body .btn_pre{ background-color: #3c9fe6;}
+#content .movie_body {
+  flex: 1;
+  overflow: auto;
+}
+.movie_body ul {
+  margin: 0 12px;
+  overflow: hidden;
+}
+.movie_body ul li {
+  margin-top: 12px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px #e6e6e6 solid;
+  padding-bottom: 10px;
+}
+.movie_body .pic_show {
+  width: 64px;
+  height: 90px;
+}
+.movie_body .pic_show img {
+  width: 100%;
+}
+.movie_body .info_list {
+  margin-left: 10px;
+  flex: 1;
+  position: relative;
+}
+.movie_body .info_list h2 {
+  font-size: 17px;
+  line-height: 24px;
+  width: 150px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.movie_body .info_list p {
+  font-size: 13px;
+  color: #666;
+  line-height: 22px;
+  width: 200px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.movie_body .info_list .grade {
+  font-weight: 700;
+  color: #faaf00;
+  font-size: 15px;
+}
+.movie_body .info_list img {
+  width: 50px;
+  position: absolute;
+  right: 10px;
+  top: 5px;
+}
+.movie_body .btn_mall,
+.movie_body .btn_pre {
+  width: 47px;
+  height: 27px;
+  line-height: 28px;
+  text-align: center;
+  background-color: #f03d37;
+  color: #fff;
+  border-radius: 4px;
+  font-size: 12px;
+  cursor: pointer;
+}
+.movie_body .btn_pre {
+  background-color: #3c9fe6;
+}
+.movie_body .pullDown {
+  margin: 0;
+  padding: 0;
+  border: none;
+}
+.movie_body .pullDown img {
+  width: 25px;
+  height: 25px;
+  margin: 0 auto;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import App from './App.vue'
 import router from './routers'
 import store from './stores'
 import axios from 'axios'
-
-Vue.prototype.axios=axios;
+import Scroll from '@/components/Scroll'
+import Loading from '@/components/Loading'
+Vue.component('Scroll', Scroll)
+Vue.component('Loading', Loading)
+Vue.prototype.axios = axios;
 Vue.config.productionTip = false
 
-Vue.filter('setWH',(url,arg)=>{
-  return url.replace(/w\.h/,arg)
+Vue.filter('setWH', (url, arg) => {
+  return url.replace(/w\.h/, arg)
 })
 
 new Vue({
